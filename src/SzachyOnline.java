5de74f8312d81bfee1f0c11d062aea7e7064c1d0
Cudakowski <wojciech.osowski.mjwn@gmail.com>
2026-04-13 11:55:43 +0200

    javaFX libraries added

---
+++ src/SzachyOnline.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SzachyOnline extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        MenuView menu = new MenuView();
+        stage.setScene(menu.createScene(stage));
+        stage.setTitle("Szachy");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+
